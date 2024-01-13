f280913a363274d585f0f3523d545b7b2ab3ae11
GBM1986 <gunnar_bjarni@hotmail.com>
2024-01-13 22:55:02 +0100

    allt ad koma

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import { Link } from 'react-scroll'
 import { FaTimes } from "react-icons/fa";
-import { CiMenuFries } from 'react-icons/ci'
+import { CiMenuBurger } from "react-icons/ci";
 
 const Navbar = () => {
     const [click, setClick] = useState(false);
-    const handleClick = () => setClick(!click);
+    const handleClick = () => {
+        setClick(prevState => !prevState);
+    };
 
     const content = (
-        <div className="absolute left-0 right-0 block w-full transition lg:hidden top-16">
+        <div className="absolute left-0 right-0 block w-full bg-white transition lg:hidden top-16 z-20">
             <ul className="p-20 text-xl text-center">
                 <Link spy={true} smooth={true} to="/"> 
                     <li className="py-4 my-4 border-b cursor-pointer border-primary hover:border-primary">Hjem</li>
@@ -28,19 +30,18 @@ const Navbar = () => {
 
     return (
         <nav>
-            <div className="z-50 flex justify-between flex-1 px-20 py-4 ml-auto h-10vh lg:py-1 mb-0.5 border-b-4 border-primary">
-                <div className="justify-start flex-1 font-inter lg:flex md:flex lg: items center">
+            <div className="z-50 flex justify-between flex-1 px-10 py-4 ml-auto h-10vh lg:py-1 mb-0.5 border-b-4 border-primary">
+                <div className="justify-start flex-1 font-inter pt-2 lg:flex md:flex lg: items center">
                     <div className="hidden flex-10 lg:flex md:flex">
                         <ul className="flex gap-8 text-[14px]">
                             <Link spy={true} smooth={true} to="/"> 
                                 <li className="pr-4 transition border-r-2 cursor-pointer text-primary border-primary hover:text-secondary ">Hjem</li>
                             </Link>
-                            
-                            <Link spy={true} smooth={true} to="advokatrerne">
+                            <Link spy={true} smooth={true} to="/advokatrerne">
                                 <li className="pr-4 transition border-r-2 cursor-pointer text-primary border-primary hover:text-secondary ">Advokaterne</li>
                             </Link>
                             <Link spy={true} smooth={true} to="omleolov">
-                                <li className="pr-4 transition border-r-2 cursor-pointer text-primary border-primary hover:text-secondary ">Om leolov</li>
+                                <li className="pr-4 transition border-r-2 cursor-pointer whitespace-pre text-primary border-primary hover:text-secondary ">Om leolov</li>
                             </Link>
                             <Link spy={true} smooth={true} to="kontakt">
                                 <li className="transition cursor-pointer text-primary border-primary hover:text-secondary">Kontakt</li>
@@ -51,12 +52,12 @@ const Navbar = () => {
                         {click && content}
                     </div>
 
-                    <button className="block transition lg:hidden md:hidden" onClick={handleClick}>
-                        {click ? <FaTimes /> : <CiMenuFries/>}
-                    </button>
+                    <button className="block transition z-50 lg:hidden md:hidden" onClick={() => setClick(!click)} style={{ zIndex: 100 }}>
+  {click ? <FaTimes /> : <CiMenuBurger />}
+</button>
                 </div>
                 <div className="flex items-center justify-end flex-1 lg:ml-4 md:ml-2">
-                    <span className="text-3xl font-light text-primary">Leo</span><p className="mr-8 text-3xl font-light">-love</p>
+                    <span className="text-3xl font-light text-primary whitespace-pre">Leo</span><p className= "whitespace-pre mr-8 text-3xl font-light">-love</p>
                 </div>
             </div>
         </nav>
--- src/pages/Hjem/Hjem.jsx
+++ src/pages/Hjem/Hjem.jsx
@@ -1,17 +1,83 @@
 import React from 'react';
-import Navbar  from '../../components/Navbar.jsx';
+import Navbar from '../../components/Navbar.jsx';
 
 function Hjem() {
-    return (
-      <div>
-        <Navbar />
-        <main className="hovedkontent">
-          <section id="banner" class="banner">
-            <img className='relative h-[100vh] w-full object-cover' src="src/Images/LeoLovOnePage/MainImage.jpg" alt="Header Image" />
-          </section>
-        </main>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Navbar />
+      <main className="hovedkontent relative">
+        <section id="banner" className="banner relative">
+          <img
+            className="absolute h-[100vh] w-full object-cover"
+            src="src/Images/LeoLovOnePage/MainImage.jpg"
+            alt="Header Image"
+          />
+        </section>
 
-  export default Hjem
+        <section className="container mx-auto px-8 py-8 lg:py-40 relative z-10 flex flex-col items-end  top-[90px] w-full">
+            <div class="text-container mb-[200px]">
+                <h2 class="block antialiased tracking-normal !text-3xl !leading-3 lg:!text-[140px] text-white">
+                  <span class='text-primary'>Leo</span>-love
+                </h2>
+                <p class="block antialiased text-xl ml-4 mt-16 leading-snu text-inherit w-full font-normal !text-white">
+                  Lov og ret kan være et plaster! Hvis du ellers har knaster!
+                </p>
+              </div>
+          <div className="mt-[260px] grid grid-cols-1 gap-8 lg:grid-cols-4">
+            <div className="relative flex flex-col bg-white text-gray-700 shadow-md  min-h-[20rem] items-end overflow-hidden rounded-xl">
+            <div className="h-80 w-full bg-white rounded-xl shadow-xl p-6 relative flex flex-col items-center justify-center">
+                <h4 className="block antialiased tracking-normal text-2xl font-semibold leading-snug text-primary mb-4">
+                  Familieret
+                </h4>
+                <p className="block antialiased text-base font-light leading-relaxed text-gray-500">
+                  Familieret er en ret alle har.
+                  Har du en familie har du sikkert ret til familieret.
+                  Medmindre du altid er den der tror du har ret.
+                  Vi desværre ikke hjælpe dig.
+                </p>
+              </div>
+            </div>
+            <div className="relative flex flex-col bg-white  text-gray-700 shadow-md relative grid min-h-[20rem] items-end overflow-hidden rounded-xl">
+            <div className="h-80 w-full bg-white rounded-xl shadow-xl p-6 relative flex flex-col items-center justify-center">
+              <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-primary text-center mb-4">
+                Ejendomsret
+              </h4>
+              <p className="block antialiased font-sans text-base font-light leading-relaxed text-gray-500 text-center">
+                Har du købt et hus og fortrudt eller er du blevet svindlet?
+                Fortvivl ej. Vi kan hjælpe dig med at komme af med huset,
+                eller eventuelt sagøge vedkommende der solgte dig det.
+              </p>
+            </div>
+            </div>
+            <div className="relative flex flex-col bg-white  text-gray-700 shadow-md relative grid min-h-[20rem] items-end overflow-hidden rounded-xl">
+            <div className="h-80 w-full bg-white rounded-xl shadow-xl p-6 relative flex flex-col items-center justify-center">
+                <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-primary text-center mb-4">
+                  Konkurs
+                </h4>
+                <p className="block antialiased font-sans text-base font-light leading-relaxed text-gray-500 text-center">
+                  Er du gået konkurs ville vi rigtig gerne hjælpe,
+                  but we cannot. We are all lawyers here with fancy cars,
+                  so if you have no money, you get no help.
+                </p>
+              </div>
+            </div>
+            <div className="relative flex flex-col bg-white  text-gray-700 shadow-md relative grid min-h-[20rem] items-end overflow-hidden rounded-xl">
+            <div className="h-80 w-full bg-white rounded-xl shadow-xl p-6 relative flex flex-col items-center justify-center">
+                <h4 className="block antialiased tracking-normal font-sans text-2xl font-semibold leading-snug text-primary text-center mb-4">
+                  Selskabsret
+                </h4>
+                <p className="block antialiased font-sans text-base font-light leading-relaxed text-gray-500 text-center">
+                  Company law. Do you never know which law to serve at a big party?
+                  Neither do we. Fortunately, we have a man abroad who knows a lot
+                  about this, so contact us today.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default Hjem;
